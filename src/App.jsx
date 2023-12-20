7505c46cfc1ee61ded3a0b832de8b08e448cc773
swathimeenal <swathimeenal1994@gmail.com>
2023-12-21 01:10:20 +0530

    sample-portfolioupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react' //rfce
+import card from './components/card'
 
 function App() {
   let data = 
@@ -24,17 +25,7 @@ function App() {
 <div className="row">
  {
   data.map((e,i)=>{
-    return<div className="col-lg-4 col-sm-6 mb-4" key={i}>
-    <div className="card h-100">
-      <a href="#"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt=""/></a>
-      <div className="card-body">
-        <h4 className="card-title">
-          <a href="#">{e.title}</a>
-        </h4>
-        <p className="card-text">{e.description}</p>
-      </div>
-    </div>
-  </div>
+    return<card carddata={e} key={i}/>
   })
   } 
   </div>
